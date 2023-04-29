39009a426bc5ccc911a10212d6b6f945460535ea
Jose <ls.joseignacio@gmail.com>
2023-04-29 17:29:25 +0200

    move functions to separate file

---
+++ dataFunctions.js
@@ -0,0 +1,71 @@
+
+import { connect } from "./db.js";
+import { Question } from "./question.js";
+
+
+const sortByDate = (questions) => {
+  return questions.sort((a, b) => new Date(b.date) - new Date(a.date));
+}
+
+export const userQuestions = async ({ query }) => {
+  const questions = await Question.find({ user: query })
+  return sortByDate(questions);
+}
+export const search = async ({ query }) => {
+  const questions = await Question.find({ $or: [{ question: { "$regex": query, "$options": "i" } }, { answer: { "$regex": query, "$options": "i" } }] })
+  return sortByDate(questions);
+}
+export const addQuestion = async ({ user, question, subscribe }) => {
+  const newQuestion = await new Question({
+    user,
+    question,
+    subscribe,
+    answer: "",
+    isFaq: false,
+    date: new Date()
+  })
+
+  const savedQuestion = await newQuestion.save();
+  //enviar email al usuario confirmando
+  return savedQuestion;
+}
+export const answerQuestion = async ({ id, answer }) => {
+  const question = await Question.findByIdAndUpdate(id, { answer })
+  // enviar email al usuario confirmando
+  return question
+}
+export const page = async ({ query, onlyUnanswered, showFaq }) => {
+  const pageLength = 10;
+  let searchQuery = {}
+  if (onlyUnanswered) {
+    searchQuery = { ...searchQuery, answer: "" }
+  }
+  if (!showFaq) {
+    searchQuery = { ...searchQuery, isFaq: false }
+  }
+  const questions = await Question.find(searchQuery);
+
+  return {
+    questions: questions.slice(query * pageLength, (query + 1) * pageLength),
+    maxPage: Math.floor(questions.length / pageLength)
+  }
+}
+export const faq = async () => {
+  const questions = await Question.find({ isFaq: true })
+  return questions
+}
+export const deleteQuestion = async ({ id }) => {
+  const question = await Question.findByIdAndDelete(id);
+  return question;
+}
+export const updateFaq = async ({ id, isFaq }) => {
+  const extra = isFaq ? {
+    date: new Date("2000/01/01"),
+    user: "admin",
+    subscribe: false
+  } : {}
+  const question = await Question.findByIdAndUpdate(id, {
+    isFaq, ...extra
+  })
+  return question
+}
--- index.js
+++ index.js
@@ -2,8 +2,7 @@ import express from "express"
 import { graphqlHTTP } from "express-graphql"
 import { buildSchema } from "graphql"
 import cors from "cors";
-import { connect } from "./db.js";
-import { Question } from "./question.js";
+import { userQuestions, search, addQuestion, answerQuestion, page, faq, deleteQuestion, updateFaq } from "./dataFunctions.js";
 
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
@@ -34,74 +33,10 @@ const schema = buildSchema(`
   }
 `)
 
-const sortByDate = (questions) => {
-  return questions.sort((a, b) => new Date(b.date) - new Date(a.date));
-}
 
 // The root provides a resolver function for each API endpoint
 const root = {
-  userQuestions: async ({ query }) => {
-    const questions = await Question.find({ user: query })
-    return sortByDate(questions);
-  },
-  search: async ({ query }) => {
-    const questions = await Question.find({ $or: [{ question: { "$regex": query, "$options": "i" } }, { answer: { "$regex": query, "$options": "i" } }] })
-    return sortByDate(questions);
-  },
-  addQuestion: async ({ user, question, subscribe }) => {
-    const newQuestion = await new Question({
-      user,
-      question,
-      subscribe,
-      answer: "",
-      isFaq: false,
-      date: new Date()
-    })
-
-    const savedQuestion = await newQuestion.save();
-    //enviar email al usuario confirmando
-    return savedQuestion;
-  },
-  answerQuestion: async ({ id, answer }) => {
-    const question = await Question.findByIdAndUpdate(id, { answer })
-    // enviar email al usuario confirmando
-    return question
-  },
-  page: async ({ query, onlyUnanswered, showFaq }) => {
-    const pageLength = 10;
-    let searchQuery = {}
-    if (onlyUnanswered) {
-      searchQuery = { ...searchQuery, answer: "" }
-    }
-    if (!showFaq) {
-      searchQuery = { ...searchQuery, isFaq: false }
-    }
-    const questions = await Question.find(searchQuery);
-
-    return {
-      questions: questions.slice(query * pageLength, (query + 1) * pageLength),
-      maxPage: Math.floor(questions.length / pageLength)
-    }
-  },
-  faq: async () => {
-    const questions = await Question.find({ isFaq: true })
-    return questions
-  },
-  deleteQuestion: async ({ id }) => {
-    const question = await Question.findByIdAndDelete(id);
-    return question;
-  },
-  updateFaq: async ({ id, isFaq }) => {
-    const extra = isFaq ? {
-      date: new Date("2000/01/01"),
-      user: "admin",
-      subscribe: false
-    } : {}
-    const question = await Question.findByIdAndUpdate(id, {
-      isFaq, ...extra
-    })
-    return question
-  }
+  userQuestions, search, addQuestion, answerQuestion, page, faq, deleteQuestion, updateFaq
 }
 
 const app = express()
